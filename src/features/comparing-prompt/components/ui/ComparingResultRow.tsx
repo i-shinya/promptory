2a29e5df1e3c7e8f84883291d734bd5b18a0aa75
i-shinya <i.shinya0376@gmail.com>
2024-02-18 16:58:11 +0900

    feat:  OpenAIへの問い合わせ時にプログレスバーを表示する

--- src/features/comparing-prompt/components/ui/ComparingResultRow.tsx
+++ src/features/comparing-prompt/components/ui/ComparingResultRow.tsx
@@ -1,4 +1,5 @@
 import IconButton from '@/components/ui/IconButton'
+import { Progress } from '@/components/ui/progress'
 import { Textarea } from '@/components/ui/textarea'
 
 interface ComparingPromptRowProps {
@@ -6,6 +7,7 @@ interface ComparingPromptRowProps {
   systemPrompt: string
   setSystemPrompt: (systemPrompt: string) => void
   deleteRow: () => void
+  isLoading?: boolean
 }
 
 const ComparingResultRow: React.FC<ComparingPromptRowProps> = ({
@@ -13,32 +15,43 @@ const ComparingResultRow: React.FC<ComparingPromptRowProps> = ({
   systemPrompt,
   setSystemPrompt,
   deleteRow: removeRow,
+  isLoading,
 }) => {
+  const progress = isLoading ? 30 : 100
+
   return (
-    <div className="flex flex-row gap-2 grow">
-      <div className="dark:bg-neutral-600 p-1 w-1/2">
-        <Textarea
-          value={systemPrompt}
-          onChange={(e) => setSystemPrompt(e.target.value)}
-          className="h-full"
-          placeholder="Enter System Prompt."
-        />
+    <>
+      <div className="flex flex-row gap-2 grow">
+        <div className="dark:bg-neutral-600 p-1 w-1/2">
+          <Textarea
+            value={systemPrompt}
+            onChange={(e) => setSystemPrompt(e.target.value)}
+            className="h-full"
+            placeholder="Enter System Prompt."
+          />
+        </div>
+        <div className="flex items-center">
+          <span className="i-solar-double-alt-arrow-right-outline"></span>
+        </div>
+        {isLoading ? (
+          <div className="p-1 w-1/2 flex items-center">
+            <Progress className="dark:bg-neutral-600" value={progress} />
+          </div>
+        ) : (
+          <div className="dark:bg-neutral-600 p-1 w-1/2 h-full">
+            <div className="dark:bg-neutral-700 h-full p-1">{answer}</div>
+          </div>
+        )}
+        <div className="flex items-center">
+          <IconButton
+            icon="i-solar-trash-bin-trash-bold"
+            onClick={() => {
+              removeRow()
+            }}
+          />
+        </div>
       </div>
-      <div className="flex items-center">
-        <span className="i-solar-double-alt-arrow-right-outline"></span>
-      </div>
-      <div className="dark:bg-neutral-600 p-1 w-1/2">
-        <div className="dark:bg-neutral-700 h-full p-1">{answer}</div>
-      </div>
-      <div className="flex items-center">
-        <IconButton
-          icon="i-solar-trash-bin-trash-bold"
-          onClick={() => {
-            removeRow()
-          }}
-        />
-      </div>
-    </div>
+    </>
   )
 }
 
